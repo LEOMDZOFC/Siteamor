d7afd72f7c707c052008e7c0bccee14575e9875f
STANLEY MODZ <leaocarvalhoscaio@gmail.com>
2025-08-27 07:24:43 -0300

    Update Script.js

--- Script.js
+++ Script.js
@@ -25,7 +25,7 @@ const body = document.querySelector("body");const swalst = Swal.mixin({timer: 23
          } else {
            await swals.fire('Ups!', 'Nama tidak boleh kosong atau lebih dari 10 karakter, ya!');inipesan();
     }*/
-    vketikutama="VC É MUITO ESPECIAL PARÁ MIN 🥰";
+    vketikutama="FELIZ ANIVERSÁRIO LAIS 🤩";
     mulainama();
   }
 
